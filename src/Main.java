fec2228b01d8d2eff2186d0b3a13f906133763d4
MatteD04 <matteo.dantonio04@gmail.com>
2025-05-25 11:46:34 +0200

    aggiunto inserimento dati da terminale

--- src/Main.java
+++ src/Main.java
@@ -37,5 +37,58 @@ public class Main {
         for (Docente d : docenti) {
             System.out.println(d);
         }
+
+        System.out.println("Quanti discenti vuoi inserire?");
+        int nDiscenti = sc.nextInt();
+        sc.nextLine();
+
+        for (int i=0; i< nDiscenti; i++){
+            System.out.println("Inserisci ID:");
+            int id = sc.nextInt(); sc.nextLine();
+            System.out.println("Inserisci nome:");
+            String nome = sc.nextLine();
+            System.out.println("Inserisci cognome:");
+            String cognome = sc.nextLine();
+            System.out.println("inserisci matricola");
+            String matricola = sc.nextLine();
+            System.out.println("Inserisci eta:");
+            int eta = sc.nextInt(); sc.nextLine();
+            System.out.println("inserisci citta_residenza");
+            String citta_residenza = sc.nextLine();
+
+
+            Discente discente = new Discente(id, nome, cognome, matricola, eta, citta_residenza);
+            discenti.add(discente);
+        };
+
+        System.out.println("\n--- DISCENTI INSERITI ---");
+        for (Discente discente : discenti) {
+            System.out.println(discente);
+        }
+
+        System.out.println("Quanti corsi vuoi inserire?");
+        int nCorsi = sc.nextInt();
+        sc.nextLine();
+
+        for (int i=0; i< nCorsi; i++){
+            System.out.println("Inserisci ID:");
+            int id = sc.nextInt(); sc.nextLine();
+            System.out.println("Inserisci nome:");
+            String nome = sc.nextLine();
+            System.out.println("Inserisci anno_accademico:");
+            String anno_accademico = sc.nextLine();
+            System.out.println("Inserisci id_docente:");
+            int id_docente = sc.nextInt(); sc.nextLine();
+
+            Corso corso = new Corso(id, nome, anno_accademico, id_docente);
+            corsi.add(corso);
+        };
+
+        System.out.println("\n--- CORSI INSERITI ---");
+        for (Corso corso : corsi) {
+            System.out.println(corso);
+        }
+
+
     }
 }
