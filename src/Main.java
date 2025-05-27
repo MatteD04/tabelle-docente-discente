4042685f62d377b2b63f2be9c5bfa20336f0ef83
MatteD04 <matteo.dantonio04@gmail.com>
2025-05-27 09:45:40 +0200

    implementato l'insert al database dei discenti

--- src/Main.java
+++ src/Main.java
@@ -16,16 +16,18 @@ public class Main {
         //Connection conn = DatabaseConfig.getConnection();
 
         /*if (conn != null) {
-            System.out.println("✅ Connessione al database riuscita!");
+            System.out.println(" Connessione al database riuscita!");
         } else {
-            System.out.println("❌ Connessione fallita.");
+            System.out.println(" Connessione fallita.");
         }*/
+
         Scanner sc = new Scanner(System.in);
 
         ArrayList<Docente> docenti = new ArrayList<>();
         ArrayList<Discente> discenti = new ArrayList<>();
         ArrayList<Corso> corsi = new ArrayList<>();
 
+        //INSERIMENTO DOCENTI
         System.out.println("Quanti docenti vuoi inserire?");
         int nDocenti = sc.nextInt();
         sc.nextLine();
@@ -49,21 +51,21 @@ public class Main {
                     String sql = "INSERT INTO docente (id, nome, cognome, data_nascita) VALUES (?, ?, ?, ?)";
                     PreparedStatement stmt = conn.prepareStatement(sql);
 
-                    stmt.setInt(1, id);               // ID inserito manualmente
-                    stmt.setString(2, nome);          // Nome
-                    stmt.setString(3, cognome);       // Cognome
-                    stmt.setDate(4, data_nascitaSQL);  // Data di nascita
+                    stmt.setInt(1, id);
+                    stmt.setString(2, nome);
+                    stmt.setString(3, cognome);
+                    stmt.setDate(4, data_nascitaSQL);
 
                     stmt.executeUpdate();
                     stmt.close();
 
-                    System.out.println("✅ Inserito nel database: " + nome + " " + cognome);
+                    System.out.println(" Inserito nel database: " + nome + " " + cognome);
                 }
             }else {
-                System.out.println("❌ Connessione al database fallita.");
+                System.out.println(" Connessione al database fallita.");
             }
         } catch (SQLException e) {
-            System.out.println("❌ Errore durante l'inserimento: " + e.getMessage());
+            System.out.println(" Errore durante l'inserimento: " + e.getMessage());
         }
 
         System.out.println("\n--- DOCENTI INSERITI ---");
@@ -71,34 +73,61 @@ public class Main {
             System.out.println(d);
         }
 
+        //INSERIMENTO DISCENTI
         System.out.println("Quanti discenti vuoi inserire?");
         int nDiscenti = sc.nextInt();
         sc.nextLine();
 
-        for (int i=0; i< nDiscenti; i++){
-            System.out.println("Inserisci ID:");
-            int id = sc.nextInt(); sc.nextLine();
-            System.out.println("Inserisci nome:");
-            String nome = sc.nextLine();
-            System.out.println("Inserisci cognome:");
-            String cognome = sc.nextLine();
-            System.out.println("inserisci matricola");
-            String matricola = sc.nextLine();
-            System.out.println("Inserisci eta:");
-            int eta = sc.nextInt(); sc.nextLine();
-            System.out.println("inserisci citta_residenza");
-            String citta_residenza = sc.nextLine();
+        try (Connection conn = DatabaseConfig.getConnection()) {
+            if (conn != null) {
+                for (int i=0; i< nDiscenti; i++){
+                    System.out.println("Inserisci ID:");
+                    int id = sc.nextInt(); sc.nextLine();
+                    System.out.println("Inserisci nome:");
+                    String nome = sc.nextLine();
+                    System.out.println("Inserisci cognome:");
+                    String cognome = sc.nextLine();
+                    System.out.println("inserisci matricola");
+                    String matricola = sc.nextLine();
+                    System.out.println("Inserisci eta:");
+                    int eta = sc.nextInt(); sc.nextLine();
+                    System.out.println("inserisci citta_residenza");
+                    String citta_residenza = sc.nextLine();
 
 
-            Discente discente = new Discente(id, nome, cognome, matricola, eta, citta_residenza);
-            discenti.add(discente);
-        };
+                    Discente discente = new Discente(id, nome, cognome, matricola, eta, citta_residenza);
+                    discenti.add(discente);
+
+                    String sql = "INSERT INTO discente (id, nome, cognome, matricola, eta, citta_residenza) VALUES (?, ?, ?, ?, ?, ?)";
+                    PreparedStatement stmt = conn.prepareStatement(sql);
+
+                    stmt.setInt(1, id);
+                    stmt.setString(2, nome);
+                    stmt.setString(3, cognome);
+                    stmt.setString(4, matricola);
+                    stmt.setInt(5, eta);
+                    stmt.setString(6, citta_residenza);
+
+                    stmt.executeUpdate();
+                    stmt.close();
+
+                    System.out.println(" Inserito nel database: " + nome + " " + cognome + " (" + " ) " + "di " + citta_residenza);
+                }
+            }else {
+                System.out.println(" Connessione al database fallita.");
+            }
+        } catch (SQLException e) {
+            System.out.println(" Errore durante l'inserimento: " + e.getMessage());
+        }
+
 
         System.out.println("\n--- DISCENTI INSERITI ---");
         for (Discente discente : discenti) {
             System.out.println(discente);
         }
 
+
+        //INSERIMENTO CORSI
         System.out.println("Quanti corsi vuoi inserire?");
         int nCorsi = sc.nextInt();
         sc.nextLine();
